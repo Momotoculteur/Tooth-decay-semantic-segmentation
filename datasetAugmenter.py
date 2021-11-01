04ca0d037eeba948f4927e9a955e1759cec77c58
Bastien MAURICE <bastien_maurice@hotmail.com>
2021-11-01 15:32:48 +0100

    ajout dinfos dans le csv dataset, data aug que pour carry

--- datasetAugmenter.py
+++ datasetAugmenter.py
@@ -7,6 +7,7 @@ import time
 from visualizer import visualize
 from PIL import Image
 import pandas as pd
+import json
 
 DIR_IMG_SRC = "data\\img\\ori"
 DIR_MASK_SRC = "data\\img\\mask"
@@ -14,14 +15,17 @@ DIR_MASK_SRC = "data\\img\\mask"
 MASK_FORMAT = ".png"
 IMG_FORMAT = ".jpg"
 
-N_IMG = len(os.listdir(DIR_IMG_SRC))
-
 N_AUG_PER_IMG = 0
 
 DATASET = pd.read_csv("data\\label\\dataset.csv", sep=',', index_col=0)
 pathDfAugmented = "data\\label\\datasetAugmented.csv"
 
 DATASET_AUGMENTED = []
+
+AUGMENT_ONLY_CARRY = True
+N_IMG = 0
+
+
 
 def askInfos():
     global N_AUG_PER_IMG
@@ -47,23 +51,61 @@ def askInfos():
     elif(confirm == "n"):
         askInfos()
 
+def get_transform(image, mask, original_h, original_w):
+    transform = A.Compose([
+        A.OneOf([
+            A.RandomSizedCrop(min_max_height=(100, 600), height=original_h, width=original_w, p=0.3),
+            A.PadIfNeeded(min_height=original_h, min_width=original_w, p=0.3)
+        ], p=1),
+        A.VerticalFlip(p=0.3),
+        A.RandomRotate90(p=0.3),
+        A.OneOf([
+            A.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03, p=0.3),
+            A.GridDistortion(p=0.3),
+            A.OpticalDistortion(distort_limit=2, shift_limit=0.5, p=0.3)
+        ], p=0.3),
+        A.CLAHE(p=0.3),
+        A.RandomBrightnessContrast(p=0.3),
+        A.RandomGamma(p=0.3)
+    ])
+    return transform
 
 def launchAugmentation():
-    transform = A.Compose([
-        A.Flip(p=0.5),
-        A.Transpose(p=0.5),
-        A.RandomRotate90(p=0.5)
-    ])
+
 
     for index, row in tqdm(DATASET.iterrows(), total=DATASET.shape[0]):
+
         rawImgPath = row['x_path'].split('.')[0]
         rawMaskPath = row['y_path'].split('.')[0]
         baseImage = cv2.imread(row['x_path'], cv2.IMREAD_COLOR)
         baseMask = cv2.imread(row['y_path'], cv2.IMREAD_GRAYSCALE)
 
+        height, width, channels = baseImage.shape
+
+        '''
+        
         for i in range(N_AUG_PER_IMG):
+
             newImgPath = rawImgPath + "_aug_{:d}".format(i) + IMG_FORMAT
             newMaskPath = rawMaskPath + "_aug_{:d}".format(i) + MASK_FORMAT
+
+            #augmented = get_transform(image=baseImage, mask=baseMask, original_h=height, original_w=width)
+            transform = A.Compose([
+                A.OneOf([
+                    A.RandomSizedCrop(min_max_height=(50, 200), height=height, width=width, p=0.3),
+                    A.PadIfNeeded(min_height=height, min_width=width, p=0.3)
+                ], p=1),
+                A.VerticalFlip(p=0.3),
+                A.RandomRotate90(p=0.3),
+                A.OneOf([
+                    A.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03, p=0.3),
+                    A.GridDistortion(p=0.3),
+                    A.OpticalDistortion(distort_limit=2, shift_limit=0.5, p=0.3)
+                ], p=0.3),
+                A.CLAHE(p=0.3),
+                A.RandomBrightnessContrast(p=0.3),
+                A.RandomGamma(p=0.3)
+            ])
 
             augmented = transform(image=baseImage, mask=baseMask)
 
@@ -71,6 +113,7 @@ def launchAugmentation():
             cv2.imwrite(newMaskPath, augmented['mask'])
 
             DATASET_AUGMENTED.append([newImgPath, newMaskPath])
+        '''
 
     #print(DATASET_AUGMENTED)
 
@@ -85,8 +128,18 @@ def launchAugmentation():
 
     globalDf.to_csv(pathDfAugmented, sep=',')
 
+
 if __name__ == "__main__":
-    askInfos()
+    if AUGMENT_ONLY_CARRY:
+        N_IMG = len(DATASET[DATASET['at_least_one_carry'] == True])
+    else:
+        N_IMG = len(DATASET)
+
+    #askInfos()
+    
+
+
+
 
 
 
